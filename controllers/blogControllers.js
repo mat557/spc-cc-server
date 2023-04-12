246b965a65285c10862c5aac51123c6f07b67e0b
mat557 <71515771+mat557@users.noreply.github.com>
2023-04-13 02:00:19 +0600

    post blog added

---
+++ controllers/blogControllers.js
@@ -0,0 +1,40 @@
+const { getDb } = require("../utils/dbConnect");const { ObjectId } = require("mongodb");
+
+
+module.exports.getAllBlogs = async(req,res) =>{
+    try{
+        const db = getDb();
+        const result = await db.collection('blogs').find().toArray();
+        res.json(result);
+
+    }catch(err){
+        res.send(err);
+    }
+}
+
+
+module.exports.postSingleBlog = async(req,res) =>{
+    try{
+        const db = getDb();
+        const data = req.body;
+        const result = await db.collection('blogs').insertOne(data);
+        res.json(result);
+
+    }catch(err){
+        res.send(err);
+    }
+}
+
+
+module.exports.deleteSingleBlog = async(req,res) =>{
+    try{
+        const db = getDb();
+        const id = req.params.id;
+        const query = { _id : ObjectId(id)};
+        const data = await db.collection('blogs').deleteOne(query);
+        res.json(data);
+    }catch(err){
+        res.send(err);
+        console.log(err);
+    }
+}
--- routes/v1/users.routes.js
+++ routes/v1/users.routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usersControllers = require('../../controllers/usersControllers.js');
 const courseController = require('../../controllers/courseController.js');
+const blogController = require('../../controllers/blogControllers.js');
 
 // all get request
 
 router.route('/').get(usersControllers.getAllUsers)
+router.route('/collection/blog').get(blogController.getAllBlogs)
 router.route('/:email').get(usersControllers.getSingleUser)
 router.route('/courses/all').get(courseController.getAllCourses)
 router.route('/course/find/:id').get(courseController.getSingleCourse)
@@ -17,15 +19,16 @@ router.route('/course/find/:id').get(courseController.getSingleCourse)
 router.route('/addUser').put(usersControllers.putSingleUser)
 router.route('/addCourse').put(courseController.putSingleCourse)
 router.route('/update/course/:id').put(courseController.updateSingleCourse)
-router.route('/course/enrole/:email').post(courseController.enroleCourse)
 
 // all delete request
 
 router.route('/delete/post/:id').delete(courseController.deleteSingleCourse)
+router.route('/delete/blog/:id').delete(blogController.deleteSingleBlog)
 
-//all patch request 
+//all post request 
 
-
+router.route('/course/enrole/:email').post(courseController.enroleCourse)
+router.route('/blog/post').post(blogController.postSingleBlog)
 
 
 module.exports =  router;
